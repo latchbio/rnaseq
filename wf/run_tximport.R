5451dc0882c18f788042995280b0b6950f0e5cca
Ayush Kamat <ayush@latch.bio>
2022-06-27 15:51:33 -0700

    it works
    Signed-off-by: Ayush Kamat <ayush@latch.bio>

--- wf/run_tximport.R
+++ wf/run_tximport.R
@@ -4,26 +4,14 @@ args <- commandArgs(trailingOnly = TRUE)
 if (length(args) == 0) {
     stop("One argument must be supplied (GTF file).n", call. = FALSE)
 }
-
-if (!require("jsonlite", quietly = TRUE)) {
-    install.packages("jsonlite")
-}
-if (!require("BiocManager", quietly = TRUE)) {
-    install.packages("BiocManager")
-}
-
-BiocManager::install("tximport")
-BiocManager::install("GenomicFeatures")
-
 library(tximport)
 library(GenomicFeatures)
 
 quant_path <- args[2]
-
-# NEEDS TO BE A ARGUMENT
 gtf_path <- args[3]
+out_path <- args[4]
 
-txdb <- makeTxDbFromGFF(gtf_path)
+txdb <- makeTxDbFromGFF(gtf_path, format = "gtf")
 k <- keys(txdb, keytype = "TXNAME")
 tx2gene <- select(txdb, k, "GENEID", "TXNAME")
 
@@ -33,4 +21,4 @@ counts <- txi$counts
 
 df <- data.frame(Name = row.names(counts), NumReads = counts)
 
-write.table(df, file = args[4], quote = FALSE, row.names = FALSE, sep = "\t")
+write.table(df, file = out_path, quote = FALSE, row.names = FALSE, sep = "\t")
